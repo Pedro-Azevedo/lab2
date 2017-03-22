cd365985e123e17da2e1ef6947d72c3247666173
Pedro-Azevedo <j.pedroazevedo@hotmail.com>
2017-03-22 21:51:41 +0000

    Update ex0201.c
    Adição de comentários

--- ex0201.c
+++ ex0201.c
@@ -7,6 +7,7 @@ int eprimo(int);
 int main()
 {
     int n=0;
+    // ciclo para imprimir os primos casados menores do que 1000
     for(n=2; n<1000; n++)
     {
         if(eprimo(n)==1 && eprimo(n+2)==1)
@@ -20,12 +21,14 @@ int main()
 int eprimo(int _n)
 {
     int isNotPrime=0, num=0, max=0;
-    max=(int)sqrt(_n);
+    max=(int)sqrt(_n); // para testar que o número é ou nao primo basta verificar se é divisível até à raiz do número
     for (num=2; num<=max; num++)
     {
         if (_n%num==0)
-            isNotPrime=1;
+            isNotPrime=1; // se não for primo ativa a a flag isNotPrime
     }
+    
+    // Retornos da função
 
     if (isNotPrime==1)
         return 0;
