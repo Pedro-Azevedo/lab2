83dcc88e927ecbf64e9dcd1c44928984a261ac66
Pedro-Azevedo <j.pedroazevedo@hotmail.com>
2017-03-24 14:09:07 +0000

    Update ex0201.c
    Adicionados comentários

--- ex0201.c
+++ ex0201.c
@@ -2,14 +2,17 @@
 #include <stdlib.h>
 #include <math.h>
 
+//Função para verificar se um número é primo
 int eprimo(int);
 
 int main()
 {
     int n=0;
+    // Ciclo para imprimir os primos casados entre 2 e 1000
     for(n=2; n<1000; n++)
     {
-        if(eprimo(n)==1 && eprimo(n+2)==1)
+        //Condição de primos casados
+        if(eprimo(n)==1 && eprimo(n+2)==1) 
             printf("%d e %d são primos casados\n", n, n+2);
     }
 
@@ -19,14 +22,18 @@ int main()
 
 int eprimo(int _n)
 {
+    // isNotPrime é a flag que deteta se um número não é primo
     int isNotPrime=0, num=0, max=0;
+    // para verificar que o número é primo basta fazer o ciclo entre 2 e a raiz do número
     max=(int)sqrt(_n);
     for (num=2; num<=max; num++)
     {
+        // Se for divisivel por algum número entre 2 e a raiz então não é primo
         if (_n%num==0)
             isNotPrime=1;
     }
 
+    // se não for primo retorna 0, se for primo retorna 1
     if (isNotPrime==1)
         return 0;
     else
